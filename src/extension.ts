2b4d7ce0770f1048308b47a3616d1afcc48fa3fe
Nickolas Theodoulou <nickolastheodoulou@hotmail.com>
2023-11-30 15:21:07 +0000

    Mvp (#1)
    * added boilerplate to find file path
    
    * added logging of current file
    
    * output code from extension
    
    * added first prompt boilerplate
    
    ---------
    
    Co-authored-by: nickt <nicktheo1@hotmail.co.uk>

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,76 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+type CodeInput = {
+    fileName: string,
+    fileContent: string
+}[];
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "code-assist" is now active!');
+function generateFileTree(dirPath: string, level: number = 0, maxDepth: number = 5): string {
+    if (level > maxDepth || !fs.existsSync(dirPath) || !fs.statSync(dirPath).isDirectory()) {
+        return '';
+    }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('code-assist.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from code-assist!');
-	});
+    let tree = '';
+    const files = fs.readdirSync(dirPath);
 
-	context.subscriptions.push(disposable);
+    for (const file of files) {
+        tree += ' '.repeat(level * 2) + file + '\n';
+        const filePath = path.join(dirPath, file);
+        if (fs.statSync(filePath).isDirectory()) {
+            tree += generateFileTree(filePath, level + 1, maxDepth);
+        }
+    }
+
+    return tree;
 }
 
-// This method is called when your extension is deactivated
+export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('code-assist.helloWorld', async () => {
+        // Ask the user for file names
+        const input = await vscode.window.showInputBox({
+            placeHolder: 'Enter relative file names separated by commas',
+        });
+
+        const codeInput: CodeInput = [];
+
+        if (input) {
+            const fileNames = input.split(',').map(file => file.trim());
+
+            fileNames.forEach(fileName => {
+                const filePath = path.join(vscode.workspace.rootPath || '', fileName);
+                try {
+                    const fileContent = fs.readFileSync(filePath, 'utf8');
+                    codeInput.push({
+                        fileName,
+                        fileContent
+                    });
+                } catch (error) {
+                    // @ts-ignore
+                    vscode.window.showErrorMessage(`Error reading file ${fileName}: ${error.message}`);
+                }
+            });
+
+            const rootDir = fileNames.length > 0 ? path.dirname(path.join(vscode.workspace.rootPath || '', fileNames[0])) : '';
+
+            const fileTree = generateFileTree(rootDir);
+            vscode.window.showInformationMessage(`
+            I'm looking to update my code to meet the following business requirements with the ticket information:
+
+            ** INSERT TICKET**
+
+            This is my code in an array of object with the structure { fileName: string, fileContent: string }: ${JSON.stringify(codeInput)}. 
+            
+            This is the file tree of my code staring with \n ${rootDir}: \n ${fileTree}. 
+            
+            Let me know if you would like to see further input from the files.
+
+            Can you offer some code solutions to meet the ticket requirements?`);
+        }
+    });
+
+    context.subscriptions.push(disposable);
+}
+
 export function deactivate() {}
