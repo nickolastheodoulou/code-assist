d1f35671312b3f598665ba8f6d08f28eaed81dd5
Nick Theodoulou <ntheodoulou@dunelm.com>
2023-12-05 23:42:17 +0000

    add processing message

--- src/extension.ts
+++ src/extension.ts
@@ -9,7 +9,8 @@ type CodeInput = {
 
 type Message = {
     data: {
-        files: string
+        files: string,
+        ticketInfo: string
     }
 }
 
@@ -38,66 +39,15 @@ export function activate(context: vscode.ExtensionContext) {
             'formView',
             'Form View',
             vscode.ViewColumn.One,
-            {
-                // Enable scripts in the webview
-                enableScripts: true
-            }
+            { enableScripts: true }
         );
 
         panel.webview.html = getFormHtml();
 
-        // Handle messages from the webview
         panel.webview.onDidReceiveMessage(
-            async message => {
-                switch (message.command) {
-                    case 'submit':
-                        const { files, ticketInfo } = message.data;
-                        // @ts-ignore
-                        const fileNames = files.split(',').map(file => file.trim());
-
-                        // @ts-ignore
-                        if (fileNames.some(fileName => !fileName)) {
-                            vscode.window.showErrorMessage('Invalid file path provided.');
-                            return;
-                        }
-
-                        const codeInput = [];
-                        for (const fileName of fileNames) {
-                            const filePath = path.join(vscode.workspace.rootPath || '', fileName);
-                            if (!fs.existsSync(filePath)) {
-                                const errorMessage = `File not found: ${fileName}`;
-                                vscode.window.showErrorMessage(errorMessage);
-                                throw new Error(errorMessage);
-                            }
-                            try {
-                                const fileContent = await fs.promises.readFile(filePath, 'utf8');
-                                codeInput.push({ fileName, fileContent });
-                            } catch (error) {
-                                // @ts-ignore
-                                const errorMessage = `Error reading file ${fileName}: ${error.message}`;
-                                vscode.window.showErrorMessage(errorMessage);
-                            }
-                        }
-
-
-                        const rootDir = path.dirname(path.join(vscode.workspace.rootPath || '', fileNames[0]));
-
-                        const fileTree = generateFileTree(rootDir);
-                        vscode.window.showInformationMessage(`
-                          I'm looking to update my code to meet the following business requirements with the ticket information:
-              
-
-                          ${ticketInfo}
-
-              
-                          This is my code in an array of object with the structure { fileName: string, fileContent: string }: ${JSON.stringify(codeInput)}. 
-                          
-                          This is the file tree of my code staring with \n ${rootDir}: \n ${fileTree}. 
-                          
-                          Let me know if you would like to see further input from the files.
-              
-                          Can you offer some code solutions to meet the ticket requirements?`);
-                        return;
+            message => {
+                if (message.command === 'submit') {
+                    processFiles(message.data, panel.webview, context);
                 }
             },
             undefined,
@@ -106,6 +56,65 @@ export function activate(context: vscode.ExtensionContext) {
     });
 
     context.subscriptions.push(disposable);
+}
+
+async function processFiles(data: { files: string, ticketInfo: string }, webview: vscode.Webview, context: vscode.ExtensionContext) {
+    const { files, ticketInfo } = data;
+    const fileNames = files.split(',').map(file => file.trim());
+
+    try {
+        await vscode.window.withProgress({
+            location: vscode.ProgressLocation.Notification,
+            title: "Processing Files",
+            cancellable: true
+        }, async (progress, token) => {
+            token.onCancellationRequested(() => {
+                vscode.window.showInformationMessage('File processing cancelled.');
+            });
+
+            const codeInput: CodeInput = [];
+
+            for (const [index, fileName] of fileNames.entries()) {
+                if (token.isCancellationRequested) {break;}
+
+                progress.report({ increment: (index / fileNames.length) * 100, message: `Processing ${fileName}` });
+
+                const filePath = path.join(vscode.workspace.rootPath || '', fileName);
+                if (!fs.existsSync(filePath)) {
+                    throw new Error(`File not found: ${fileName}`);
+                }
+
+                const fileContent = await fs.promises.readFile(filePath, 'utf8');
+                codeInput.push({ fileName, fileContent });
+            }
+
+            if (!token.isCancellationRequested) {
+                const rootDir = path.dirname(path.join(vscode.workspace.rootPath || '', fileNames[0]));
+                const fileTree = generateFileTree(rootDir);
+
+                vscode.window.showInformationMessage(`
+                    Processing complete. 
+                `);    
+                vscode.window.showInformationMessage(`
+                I'm looking to update my code to meet the following business requirements with the ticket information:
+    
+
+                ${ticketInfo}
+
+    
+                This is my code in an array of object with the structure { fileName: string, fileContent: string }: ${JSON.stringify(codeInput)}. 
+                
+                This is the file tree of my code staring with \n ${rootDir}: \n ${fileTree}. 
+                
+                Let me know if you would like to see further input from the files.
+    
+                Can you offer some code solutions to meet the ticket requirements?`);
+            }
+        });
+    } catch (error) {
+        // @ts-ignore
+        vscode.window.showErrorMessage(error.message);
+    }
 }
 
 function getFormHtml(): string {
