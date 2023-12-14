984e68875589c044c3e62dc681e4b4a326047f06
nickolastheodoulou <nickolastheodoulou@hotmail.com>
2023-12-14 08:53:29 +0000

    add some integration tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,82 @@
-import * as assert from "assert";
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from "vscode";
-// import * as myExtension from '../../extension';
+import * as assert from "assert";
+import { CodeAssistTreeProvider } from "../views/treeView/treeView";
+import { activate, deactivate } from "../extension";
+
+function createMockExtensionContext(): vscode.ExtensionContext {
+  return {
+    subscriptions: [],
+    extensionUri: vscode.Uri.file(__dirname),
+    storageUri: vscode.Uri.file(__dirname),
+    globalStorageUri: vscode.Uri.file(__dirname),
+    logUri: vscode.Uri.file(__dirname),
+    extensionPath: __dirname,
+    asAbsolutePath: (relativePath: string) => relativePath,
+    storagePath: __dirname,
+    globalStoragePath: __dirname,
+    logPath: __dirname,
+    workspaceState: {
+      get: () => {},
+      // @ts-ignore
+      update: () => {},
+    },
+    globalState: {
+      get: () => {},
+      // @ts-ignore
+      update: () => {},
+    },
+    secrets: {
+      // @ts-ignore
+      get: () => {},
+      // @ts-ignore
+      store: () => {},
+    },
+    environmentVariableCollection: {
+      replace: () => {},
+      append: () => {},
+      prepend: () => {},
+      // @ts-ignore
+      get: () => {},
+      forEach: () => {},
+      delete: () => {},
+    },
+    // Add other necessary properties and methods here
+  };
+}
 
 suite("Extension Test Suite", () => {
   vscode.window.showInformationMessage("Start all tests.");
 
-  test("Sample test", () => {
-    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+  test("Extension Activation", async () => {
+    const mockContext = createMockExtensionContext();
+    await activate(mockContext);
+    const commands = await vscode.commands.getCommands(true);
+    assert.ok(commands.includes("code-prompt-assist.openForm"));
+    assert.ok(commands.includes("code-prompt-assist.openSettings"));
+  });
+
+  test("Tree View Provider", async () => {
+    const treeView = vscode.window.createTreeView("codeAssistTreeView", {
+      treeDataProvider: new CodeAssistTreeProvider(),
+    });
+    assert.ok(treeView);
+  });
+
+  test("Open Form Command", async () => {
+    await vscode.commands.executeCommand(
+      "code-prompt-assist.openForm",
+      "somePromptType"
+    );
+    // Additional verification for form opening can be added here
+  });
+
+  test("Open Settings Command", async () => {
+    await vscode.commands.executeCommand("code-prompt-assist.openSettings");
+    // Additional verification for settings opening can be added here
+  });
+
+  test("Extension Deactivation", async () => {
+    deactivate();
+    // Additional verification for deactivation can be added here
   });
 });
