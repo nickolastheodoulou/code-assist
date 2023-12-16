71dfcb3533bd4175524e73692a5033a376a290b3
nickolastheodoulou <nickolastheodoulou@hotmail.com>
2023-12-16 11:59:03 +0000

    wip

--- src/integration-tests/extension.test.ts
+++ src/integration-tests/extension.test.ts
@@ -75,6 +75,64 @@ suite("Extension Test Suite", () => {
     // Additional verification for settings opening can be added here
   });
 
+  test("Add and Delete Redaction Rule via Button Clicks", async () => {
+    // Activate the extension
+    const mockContext = createMockExtensionContext();
+    await activate(mockContext);
+
+    // Open the settings view
+    const webViewPanel = await vscode.commands.executeCommand("code-prompt-assist.openSettings");
+
+    // Simulate adding a rule
+    const originalText = "testOriginal";
+    const replacementText = "testReplacement";
+
+    webViewPanel.webview.postMessage({
+      command: 'setInputValues',
+      originalText: originalText,
+      replacementText: replacementText
+    }) as unknown as vscode.WebviewPanel;
+
+    // Simulate button click to add the rule
+    webViewPanel.webview.postMessage({
+      command: 'clickButton',
+      selector: '#addRule'
+    });
+
+    // Delay to ensure DOM updates
+    await new Promise(resolve => setTimeout(resolve, 1000)); // Adjust timing as needed
+
+    // Retrieve the updated rules list
+    let rules: any[] = [];
+    webViewPanel.webview.onDidReceiveMessage(
+      message => {
+        if (message.command === 'getRulesList') {
+          rules = message.data;
+        }
+      }
+    );
+    webViewPanel.webview.postMessage({ command: 'requestRulesList' });
+
+    // Verify rule addition
+    assert.ok(rules.some(rule => rule.original === originalText && rule.replacement === replacementText), "Rule not found in rules list after addition");
+
+    // Simulate button click to delete the rule
+    // You might need to dynamically determine the selector for the delete button of the specific rule
+    webViewPanel.webview.postMessage({
+      command: 'clickButton',
+      selector: '.deleteButton' // Update this selector to target the specific delete button for the added rule
+    });
+
+    // Delay to ensure DOM updates
+    await new Promise(resolve => setTimeout(resolve, 1000)); // Adjust timing as needed
+
+    // Retrieve the updated rules list
+    webViewPanel.webview.postMessage({ command: 'requestRulesList' });
+
+    // Verify rule deletion
+    assert.ok(!rules.some(rule => rule.original === originalText), "Rule found in rules list after deletion");
+  });
+
   test("Extension Deactivation", async () => {
     deactivate();
     // Additional verification for deactivation can be added here
